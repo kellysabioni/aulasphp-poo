5f8bff587efb8ea3a3244b8d20b15a0a2d4e1cc0
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-04-10 10:19:22 -0300

    Atualiza para encapsulamento

--- src/Cliente.php
+++ src/Cliente.php
@@ -1,21 +1,7 @@
 <?php
 class Cliente
 {
-    // Atributos (características da Classe)
-    public string $nome;
-    public int $idade;
-    public string $email;
-
-    // Métodos (comportamentos da Classe)
-    public function exibirDados(): void 
-    {
-        echo "<p style='color:red'>
-                Este é o cliente... $this->nome</p>";
-    }
-
-
-    public function verificarIdade(int $idade): bool
-    {
-        return true;
-    }
+    private string $nome;
+    private int $idade;
+    private string $email;
 }
