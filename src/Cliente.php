f3bc4342ce591fe209de112395cf57724ff5cdf9
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-04-10 10:45:00 -0300

    Cria metodos getters

--- src/Cliente.php
+++ src/Cliente.php
@@ -5,7 +5,7 @@ class Cliente
     private int $idade;
     private string $email;
 
-    /* Métodos setters */
+    /* Métodos setters: responsável por receber/atribuir dados para as propriedades/atributos */
     public function setNome(string $nome):void
     {
         $this->nome = $nome;
@@ -17,5 +17,19 @@ class Cliente
     public function setEmail(string $email):void
     {
         $this->email = $email;
+    }
+
+    /* Métodos getters: responsáveis por acessar/ler os dados */
+    public function getNome():string
+    {
+        return $this->nome;
+    }
+    public function getIdade():int
+    {
+        return $this->idade;
+    }
+    public function getEmail():string
+    {
+        return $this->email;
     }
 }
