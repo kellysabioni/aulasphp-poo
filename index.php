f8e56803e8bb1f8a19803bf40cc2200bedb8dd6e
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-04-15 11:24:16 -0300

    Atualiza retirando idade

--- index.php
+++ index.php
@@ -3,37 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 03</title>
+    <title>Exemplo 04</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 03</h1>
+    <h1>PHP com POO - Exemplo 04</h1>
     <hr>
 
 <?php
 require_once "src/Cliente.php";
-
-$clienteA = new Cliente("Fulano", 30, "fulano@gmail.com");
-$clienteB = new Cliente("Ozzy", 15, "ozzy@gmail.com");
-
 ?>
 
 <h2>Acessando/lendo os dados dos objetos</h2>
 
 
-<ul>
-    <li><b>Nome:</b> <?=$clienteA->getNome()?> </li>
-    <li><b>Idade:</b> <?=$clienteA->getIdade()?> </li>
-    <li><b>E-mail:</b> <?=$clienteA->getEmail()?> </li>
-    <li><b>Faixa Etária:</b> <?=$clienteA->verificarIdade()?> </li>
-</ul>
-
-
-<ul>
-    <li><b>Nome:</b> <?=$clienteB->getNome()?> </li>
-    <li><b>Idade:</b> <?=$clienteB->getIdade()?> </li>
-    <li><b>E-mail:</b> <?=$clienteB->getEmail()?> </li>
-    <li><b>Faixa Etária:</b> <?=$clienteB->verificarIdade()?> </li>
-</ul>
 
 
 <h2>Testes de acesso aos recursos 
--- src/Cliente.php
+++ src/Cliente.php
@@ -2,21 +2,13 @@
 class Cliente
 {
     private string $nome;
-    private int $idade;
     private string $email;
 
     // Método Construtor (sempre é executado automaticamente ao criar objeto)
     public function __construct(string $nome,int $idade,string $email)
     {
         $this->setNome($nome);
-        $this->setIdade($idade);
         $this->setEmail($email);
-    }
-
-    public function verificarIdade():string{
-        if ($this->idade <18) return "menor de idade";
-        if ($this->idade <60) return "adulto";
-        return "idoso";
     }
 
     public function setNome(string $nome):void
@@ -24,13 +16,6 @@ class Cliente
         $this->nome = $nome;
     }
    
-    public function setIdade(string $idade):void
-    {
-        if ($idade < 0 ) {
-            throw new InvalidArgumentException("Idade não pode ser menor que 0");
-        }
-        $this->idade = $idade;
-    }
     public function setEmail(string $email):void
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -40,15 +25,12 @@ class Cliente
         $this->email = $email;
     }
 
-    /* Métodos getters: responsáveis por acessar/ler os dados */
+
     public function getNome():string
     {
         return $this->nome;
     }
-    public function getIdade():int
-    {
-        return $this->idade;
-    }
+
     public function getEmail():string
     {
         return $this->email;
