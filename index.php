8ef6fa61a4b3a72c104d3c5f98d20adbec1146c5
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-04-17 11:30:23 -0300

    Organiza pastas em SRC

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
     <hr>
 
 <?php
-require_once "src/Situacao.php"; // Enumeração
-require_once "src/Cliente.php"; // Super Classe
-require_once "src/PessoaFisica.php"; // Sub Classe
-require_once "src/PessoaJuridica.php"; // Sub Classe
+require_once "src/Enums/Situacao.php"; // Enumeração
+require_once "src/Models/Cliente.php"; // Super Classe
+require_once "src/Models/PessoaFisica.php"; // Sub Classe
+require_once "src/Models/PessoaJuridica.php"; // Sub Classe
 
 $clientePF = new PessoaFisica("Kelly", "kelly.sabioni@gmail.com", 15, "222.222.222-22"); 
 $clientePJ = new PessoaJuridica("João Pedro", "jp@gmail.com", "12.123.123/0001-45", 2019, "Corporação Tabajara"); 
