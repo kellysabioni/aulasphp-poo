5509c30c60594f858cb11bd2541f5659c669b149
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-04-29 10:35:22 -0300

    Atualiza REQUIRE AUTOLOAD em conflitos

--- conflitos.php
+++ conflitos.php
@@ -13,8 +13,7 @@
 use Fornecedores\Pagamento;
 use Prestadores\Pagamento as PrestadoresPagamento;
 
-require_once "src/Fornecedores/Pagamento.php";
-require_once "src/Prestadores/Pagamento.php";
+require_once "vendor/autoload.php";
 
 // Criando objetos para usar as classes de pagamento
 $pagamentoFornecedor = new Pagamento();
